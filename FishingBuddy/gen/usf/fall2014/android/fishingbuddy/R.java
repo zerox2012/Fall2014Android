3ba7a073c6f71068f34e65f82a68a604b0334ca2
Huy Ton <zerox2012@yahoo.com>
2014-10-19 16:33:21 -0400

    All activity added
    now we just need to make some buttons/interface on each activity and
    link them, please check the pdf report to see what each of them should
    look like.

--- FishingBuddy/gen/usf/fall2014/android/fishingbuddy/R.java
+++ FishingBuddy/gen/usf/fall2014/android/fishingbuddy/R.java
@@ -1192,11 +1192,23 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_login=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_forgot_password=0x7f030018;
+        public static final int activity_gallery=0x7f030019;
+        public static final int activity_login=0x7f03001a;
+        public static final int activity_main_menu=0x7f03001b;
+        public static final int activity_map=0x7f03001c;
+        public static final int activity_message=0x7f03001d;
+        public static final int activity_new_user=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
-        public static final int login=0x7f0c0000;
+        public static final int forgot_password=0x7f0c0000;
+        public static final int gallery=0x7f0c0001;
+        public static final int login=0x7f0c0002;
+        public static final int main_menu=0x7f0c0003;
+        public static final int map=0x7f0c0004;
+        public static final int message=0x7f0c0005;
+        public static final int new_user=0x7f0c0006;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1242,6 +1254,12 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_forgot_password=0x7f0a0014;
+        public static final int title_activity_gallery=0x7f0a0011;
+        public static final int title_activity_main_menu=0x7f0a0010;
+        public static final int title_activity_map=0x7f0a0015;
+        public static final int title_activity_message=0x7f0a0012;
+        public static final int title_activity_new_user=0x7f0a0013;
     }
     public static final class style {
         /** 
