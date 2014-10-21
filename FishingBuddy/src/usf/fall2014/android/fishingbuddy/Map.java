43f85964fb78d40a4b0286105a74d6f393732ac6
Huy Ton <zerox2012@yahoo.com>
2014-10-20 23:23:53 -0400

    update 3
    map screen done
    message screen done
    gallery screen done
    linking all buttons done

--- FishingBuddy/src/usf/fall2014/android/fishingbuddy/Map.java
+++ FishingBuddy/src/usf/fall2014/android/fishingbuddy/Map.java
@@ -1,9 +1,13 @@
 package usf.fall2014.android.fishingbuddy;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Map extends ActionBarActivity {
 
@@ -11,24 +15,18 @@ public class Map extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		
+		 Button message = (Button) this.findViewById(R.id.button_message);
+		 OnClickListener message_clickEvent = new OnClickListener(){
+	        	public void onClick(View arg0){
+	        		Intent myIntent = new Intent(arg0.getContext(), Message.class);
+	        		Bundle bundle = new Bundle();
+	       		
+	        		myIntent.putExtras(bundle);
+	        		startActivityForResult(myIntent, 0);
+				}
+	        };
+	       message.setOnClickListener(message_clickEvent);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
